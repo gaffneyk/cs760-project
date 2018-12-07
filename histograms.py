80b6dec79552d22e6406c517afa0bc5de7503c01
zubeyr94 <eryilmaz@wisc.edu>
2018-12-06 22:12:40 -0600

    queries fixed and histogram.py finished

--- histograms.py
+++ histograms.py
@@ -25,13 +25,14 @@ def create_histograms(sql_dir):
 		for sql_file in sql_files:
 			with open(os.path.join(sql_dir, sql_file), 'r') as f:
 				sql = f.read()
-				node = parser.parse(sql)
-				join_predicates = ast.get_join_predicates(node)
+				node = parser.parse(sql) 
+
+				join_predicates = [p.to_sql() for p in ast.get_joins(node)]				
 				for join in join_predicates:
 					for attribute in join.split('='):
 						all_attributes.add(attribute)
-
-				selection_predicates= ast.get_selection_predicates(node)
+	
+				selection_predicates= [p.to_sql() for p in ast.get_selections(node)]
 				for predicate in selection_predicates:
 					all_attributes.add(predicate.split(' ')[0]);
 
@@ -39,43 +40,44 @@ def create_histograms(sql_dir):
 
 		all_attributes = sorted(all_attributes)
 
+		print(all_attributes)
 		#print(all_attributes)
 
 		buckets = 10
-		
+		return
 		# for each predicate attribute create the equi depth histogram by invoking the database
-		#for attribute in all_attributes:
-		attribute = 'company_type.kind'
-		attributeTuple = attribute.split('.')
-		relation = attributeTuple[0]
-		attr = attributeTuple[1]
+		for attribute in all_attributes:
+			#attribute = 'company_type.kind'
+			attributeTuple = attribute.split('.')
+			relation = attributeTuple[0]
+			attr = attributeTuple[1]
 
-		sqlTemplate =	'SELECT ntile, min(_attr_), max(_attr_)\
-				FROM(SELECT _attr_, ntile(_buckets_) over (order by _attr_) from _relation_) x\
-				GROUP BY ntile\
-				ORDER BY ntile'
-		sql = sqlTemplate.replace("_attr_", attr).replace("_relation_", relation).replace("_buckets_", str(buckets))
+			sqlTemplate =	'SELECT ntile, min(_attr_), max(_attr_)\
+					FROM(SELECT _attr_, ntile(_buckets_) over (order by _attr_) from _relation_) x\
+					GROUP BY ntile\
+					ORDER BY ntile'
+			sql = sqlTemplate.replace("_attr_", attr).replace("_relation_", relation).replace("_buckets_", str(buckets))
 
-		# create the histogram
-		print('PostgreSQL database version:')
-		cur.execute(sql)
+			# create the histogram
+			print('PostgreSQL database version:')
+			cur.execute(sql)
 
-		attribute = relation+'.'+attr;
-		#features = {}
-		features = {attribute:[]}
-		# display the histogram
-		for line in cur:
-			bucket = line[0]
-			limits = (line[1], line[2])
-			features[attribute].append(limits) 
-			print(line)
+			attribute = relation+'.'+attr;
+			#features = {}
+			features = {attribute:[]}
+			# display the histogram
+			for line in cur:
+				bucket = line[0]
+				limits = (line[1], line[2])
+				features[attribute].append(limits) 
+				print(line)
 	
 		#print(cur.fetchal)#(attrname)(bucket number)(limit low:0 high:1)
 
 		# close the communication with the PostgreSQL
 		cur.close()
 		
-		return features
+		json.dump(features,open('histograms.json', 'w'))
 	except (Exception, psycopg2.DatabaseError) as error:
 		print(error)
 	finally:
